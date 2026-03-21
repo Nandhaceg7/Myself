5a091eaa24ba4571a130dcf43ca55ae195e18477
Nandha <nandhaceg10@gmail.com>
2026-03-21 11:38:21 +0530

    Remove hardcoded API URL from App.jsx

--- formyuse/Front-End/foruse/src/App.jsx
+++ formyuse/Front-End/foruse/src/App.jsx
@@ -13,7 +13,7 @@ import {
 } from "lucide-react";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "";
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
